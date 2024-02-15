a73ff138bc08b832a19f9860a6e21733b8c46f35
udongdong <kimdongkyun0411@gmail.com>
2024-02-16 00:37:33 +0900

    Linked list - 2

--- Assignment.py
+++ Assignment.py
@@ -1,29 +1,69 @@
+import random
+
+
 class Node:
     def __init__(self):
         self.data = None
         self.link = None
 
 
-def print_arr(Head):
-    print(Head.data, end='')
-    while Head.link is not None:
-        Head = Head.link
-        print(Head.data, end='')
-    print()
+def insert_node(_head, _data):
+    global cnt, head
+
+    if _head.link is None:
+        if _head.data == _data:
+            pass
+        elif _data < _head.data:
+            new_node = Node()
+            new_node.data = _data
+            new_node.link = _head
+            head = new_node
+            cnt += 1
+        else:
+            new_node = Node()
+            new_node.data = _data
+            _head.link = new_node
+            cnt += 1
+
+    else:
+        while _head.link is not None:
+            if _data == _head.data:
+                break
+
+            if _data < _head.data:
+                new_node = Node()
+                new_node.data = _data
+                new_node.link = _head
+                if head == _head:
+                    head = new_node
+                cnt += 1
+                break
+            else:
+                if _data < _head.link.data:
+                    new_node = Node()
+                    new_node.data = _data
+                    new_node.link = _head.link
+                    _head.link = new_node
+                    cnt += 1
+                    break
+                else:
+                    _head = _head.link
 
 
-current = None
+def print_list(_head):
+    print(_head.data, end=' ')
+    while _head.link is not None:
+        _head = _head.link
+        print(_head.data, end=' ')
 
-name = input("Name--> ")
-while name != '':
-    mail = input("Mail--> ")
 
-    new_node = Node()
-    new_node.data = [name, mail]
+new_node = Node()
+new_node.data = random.randint(1, 45)
+head = new_node
+cnt = 1
 
-    if current is not None:
-        new_node.link = current
-    current = new_node
-    print_arr(current)
+while cnt != 6:
+    new_data = random.randint(1, 45)
+    insert_node(head, new_data)
 
-    name = input("Name--> ")
+print_list(head)
